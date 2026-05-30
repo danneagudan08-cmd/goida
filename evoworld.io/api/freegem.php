befb274e09f115d87b164d7eee1d4e6ded7c0068
danneagudan08-cmd <dan.neagudan08@gmail.com>
2026-05-30 11:20:55 +0200

    Expand API response structure for freegem endpoint

--- evoworld.io/api/freegem.php
+++ evoworld.io/api/freegem.php
@@ -1,1 +1,163 @@
-{"success":true}
+{
+    "success": true,
+    "user": {
+        "loadFiles": [],
+        "guest": true,
+        "id": 117248667,
+        "login": "Guest_118525290",
+        "loginDisplay": "Guest_118525290",
+        "name": "Guest_118525290",
+        "accountType": 0,
+        "level": 0,
+        "premiumPoints": 9950,
+        "experience": 0,
+        "experienceNextLevel": 1000,
+        "experiencePercents": 0,
+        "bonusExp": 0,
+        "premium": 3,
+        "startPoints": 3,
+        "spawnTimer": 0,
+        "discordLogged": false,
+        "access": 0,
+        "countryCode": "IT",
+        "showPremiumOffer": true,
+        "skins": "pets,1,2,3",
+        "selectedSkins": "",
+        "profileTheme": 0,
+        "profileKey": "af6294a6490eb4a9000f622f7e6e2da6",
+        "authData": {
+            "key": "h9EzvnO\/F6KmsaslkGQ21oAXyEoXJsO6\/14Zvju0RH80Cy3S1QH+e3yPRjH3WT8Lo8af3Qzp4KuU1mndBGP2fZMGiYZ0KYdLLJz\/kXV+FRs5VL8SIrWr2YGySW+10eBQS5+FvYrRj\/gXFi7Hu76xnQdKpCnaRr\/+0YSVjzjfppNtUNZScN1O2\/R1mMcQckgLpKu5BvzFjTIe179i7sqItJEJrHSI9r4yGfCXOl7ONU7Dki7Bq6HITdxQzgttQMaw1CKvSngOv0MDQjBu51AEdB5B4Oe6lFasQavYbfE8nTreALP\/wYOLBTuANDEpwQ1esPIyuaviUTVrkU9yfcBbhQ==",
+            "iv": "9UuaADflPee12vPPQ4BdPg==",
+            "data": "pZBjq7UYlLNBOKfFEkFPHwrW83sS9JexS2krsG8zNcQMIcIwSJ05PSwIfAn60O+lGy9jPZ\/dxxcTOWT\/SGLlOfg1RrSaQeafLp6oS5wQsynfrcu4TOaUz\/aFJY+TshVE"
+        }
+    },
+    "dailyQuests": {
+        "1": {
+            "id": 380356372,
+            "type": 1,
+            "amount": 10,
+            "what": "player",
+            "reward": 5000,
+            "progress": 0
+        },
+        "2": {
+            "id": 380356373,
+            "type": 5,
+            "amount": 5,
+            "what": "poo",
+            "reward": 1750,
+            "progress": 0
+        },
+        "3": {
+            "id": 380356374,
+            "type": 4,
+            "amount": 1,
+            "what": "grimReaper",
+            "reward": 302500,
+            "progress": 0
+        }
+    },
+    "popups": [],
+    "challenge": {
+        "salt": "1780132791.6172-117248667-72cf6e8f11a7932ca39454447c65bd8b80ef11628a8774a916ff92ceb45f9f20",
+        "difficulty": 3
+    },
+    "servers": [
+        {
+            "name": "North-East Europe 1",
+            "ip": "wss:\/\/135-136-6-10.evoworld.io",
+            "port": "31830",
+            "location": "EUNE",
+            "players": 254,
+            "maxPlayers": 300
+        },
+        {
+            "name": "North-East Europe 2",
+            "ip": "wss:\/\/135-136-6-10.evoworld.io",
+            "port": "32830",
+            "location": "EUNE",
+            "players": 15,
+            "maxPlayers": 300
+        },
+        {
+            "name": "West Europe 1",
+            "ip": "wss:\/\/147-135-130-181.evoworld.io",
+            "port": "31830",
+            "location": "EUW",
+            "players": 300,
+            "maxPlayers": 300
+        },
+        {
+            "name": "West Europe 3",
+            "ip": "wss:\/\/147-135-130-181.evoworld.io",
+            "port": "32830",
+            "location": "EUW",
+            "players": 122,
+            "maxPlayers": 300
+        },
+        {
+            "name": "North America 1",
+            "ip": "wss:\/\/199-231-213-9.evoworld.io",
+            "port": "31830",
+            "location": "NA",
+            "players": 89,
+            "maxPlayers": 300
+        },
+        {
+            "name": "South America 2",
+            "ip": "wss:\/\/189-1-168-53.evoworld.io",
+            "port": "31830",
+            "location": "SA",
+            "players": 68,
+            "maxPlayers": 300
+        },
+        {
+            "name": "East Asia 1",
+            "ip": "wss:\/\/45-250-255-82.evoworld.io",
+            "port": "31830",
+            "location": "ASE",
+            "players": 139,
+            "maxPlayers": 300
+        },
+        {
+            "name": "South-East Asia 1",
+            "ip": "wss:\/\/15-235-181-25.evoworld.io",
+            "port": "31830",
+            "location": "ASSE",
+            "players": 245,
+            "maxPlayers": 300
+        },
+        {
+            "name": "South-East Asia 2",
+            "ip": "wss:\/\/15-235-181-25.evoworld.io",
+            "port": "32830",
+            "location": "ASSE",
+            "players": 20,
+            "maxPlayers": 300
+        },
+        {
+            "name": "South Asia 2",
+            "ip": "wss:\/\/148-113-47-158.evoworld.io",
+            "port": "31830",
+            "location": "ASS",
+            "players": 104,
+            "maxPlayers": 300
+        },
+        {
+            "name": "West Asia 1",
+            "ip": "wss:\/\/146-70-241-26.evoworld.io",
+            "port": "31830",
+            "location": "ASW",
+            "players": 86,
+            "maxPlayers": 300
+        },
+        {
+            "name": "Australia 1",
+            "ip": "wss:\/\/139-99-208-195.evoworld.io",
+            "port": "31830",
+            "location": "AUS",
+            "players": 81,
+            "maxPlayers": 300
+        }
+    ]
+}
