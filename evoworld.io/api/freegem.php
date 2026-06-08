be948061e9be75277dd3bfb176e254dd09fd60ad
danneagudan08-cmd <dan.neagudan08@gmail.com>
2026-06-08 22:18:14 +0200

    Update freegem.php

--- evoworld.io/api/freegem.php
+++ evoworld.io/api/freegem.php
@@ -30,6 +30,12 @@
     "newEmail": "dolilomachinemoon2@gmail.com",
     "newEmailDate": "2026-06-23 17:17:57",
     "skins": "pets,1,2,3,9;grimReaper,6,4,22,52,110,1;ghostlyReaper,3,4,17;pumpkinGhost,4,6;kill_effects,2;ghost,2,26;evolution_effects,3",
+    "profileKey": "816164b073d53337fc522b5ad2b51ff7",
+    "authData": {
+      "key": "huyagnpX+cDZvqn+oH6gtfDO7V/pKPNTpI9yyewEkYopCFKWQMzWh3olSWUkdgG9OBaWL43v89cLDkqsnrgG15bs1Jt4KtVf9ndiM3klTl1kEgplwiukdAniaTtwsXSjeaUfCq3iiF5d+169B5AC1D5C8ZmmNqDxO0PfKUs03SRPYhZd69VrsxU2gH1YQGi3C6nviJqxtDeNYiWbMVulpvOsp93PfXNRIzMXCa0JoBkBGINPTYuTCy/zJ3LRkc3G6jRh6YjmcaBQUoNwJG2xo5Gf+Rz4L61qLg4HvknOM0O4+1ruC8Rz+CXTwyqHMWRXrdGl6TMK5mdZC9vKI3WPLA==",
+      "iv": "9EIsBmL71CHs9oUn/pC34Q==",
+      "data": "yBSObL8ypGablO1rJ4t2b1QlMcMFhpnrrDSgsgOMeAuChk/kXTScJ2shyj7algw0cHzvrIocLPh5F7/EEDeIuB3Hapm35kU4S0P+jvfa5DrvLI6AS8/ORAxjxbKjsmh9sMaaXNUY33Z/FY5KDauX5OMhpKF3B3oCyxOhLImJMWSQvHNrjkKG/nmI/s48ufVju0/B5NB7BCG2nG9wzckrfN7r6G5nyGHta64hDfiR8J7OzqwUjv8nvA/mOLUAINHnkv1UhtTyQoUrlKuihHf5x07gTLbTk6HAsbTx8RVh9HQ+uviI+tIhwiercbvPZ0Bq"
+    },
     "hideFlag": true,
     "selectedSkins": "pumpkinGhost:6,ghost:26,ghostlyReaper:4,grimReaper:6,pets:9,evolution_effects:3,kill_effects:2"
   },
